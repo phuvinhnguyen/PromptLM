f938ca29a993610d144947e8f86f9019f5f60d23
phuvinhnguyen <phuvinhnguyen812002@gmail.com>
2025-03-20 15:11:59 +0100

    dbug: gemma3

---
+++ SCoT/experiments/Swelite_Gemma31b_quantizehg_test.py
@@ -0,0 +1,38 @@
+import os
+import json
+from FlowDesign.chatbot.template import ImBot
+from SCoT.SCoTD import SCoTD
+from SCoT.eval.swelite import *
+import argparse
+
+class LrmBot(ImBot):
+    prompt_template = '{history}<|im_start|>assistant\n<think>\n'
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--token", type=str, help="Authentication token", default='token')
+    args = parser.parse_args()
+    
+    result_dir = "./result"
+    os.makedirs(result_dir, exist_ok=True)
+    base_filename = f"experiment_{os.path.basename(__file__)}_v0.json"
+    filepath = os.path.join(result_dir, base_filename)
+
+    print('-'*10, 'start loading chatbot', '-'*100)
+    bot = LrmBot(args.token, 'google/gemma-3-1b-it', run_local='hg', max_new_tokens=2048, device_map='auto')
+    print('-'*10, 'create agent', '-'*50)
+    agent = SCoTD(bot)
+    print('-'*10, 'run agent', '-'*50)
+    data = generate_patches(test_dataset, agent, 'SCoT_Gemma31b')
+    print('-'*10, 'finish', '-'*50)
+
+    version = 1
+    while os.path.exists(filepath):
+        filepath = os.path.join(result_dir, f"experiment_{os.path.basename(__file__)}_v{version}.json")
+        version += 1
+
+    # Save JSON data
+    with open(filepath, "w", encoding="utf-8") as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
+
+    print(f"Saved results to: {filepath}")
---
+++ scripts/slurm/SCoT_SweliteGemma31b_hg_test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=swebench_eval      # Job name
+#SBATCH --output=swebench_eval.out    # Output file
+#SBATCH --error=swebench_eval.err     # Error log file
+#SBATCH --time=02:00:00               # Time limit (hh:mm:ss)
+#SBATCH --ntasks=1                    # Number of tasks (usually 1)
+#SBATCH --cpus-per-task=4             # Number of CPU cores per task
+#SBATCH --mem=32G                     # Memory per node
+#SBATCH --gres=gpu:2                  # Request 1 GPU (remove if not needed)
+#SBATCH --partition=amperenodes           # Partition name (adjust based on cluster)
+
+# Run the first experiment
+python -m SCoT.experiments.Swelite_Gemma31b_quantizehg_test
+
+# # Clone and install SWE-bench
+# if [ ! -d "SWE-bench" ]; then
+#     git clone https://github.com/SWE-bench/SWE-bench.git
+# fi
+# cd SWE-bench
+# pip install -e .
+
+# # Run evaluation
+# python -m swebench.harness.run_evaluation \
+#     --dataset_name princeton-nlp/SWE-bench_Lite \
+#     --predictions_path "../result/experiment_Swelite_QwQ32b_test_v0.json" \
+#     --run_id validate-Swelite_QwQ32b_test \
+#     --modal true
